52678b621b153256a38597a791e2dfea6363df3c
Katherine Stehberg <kstehberg@gmail.com>
2024-06-21 21:06:38 -0400

    changed index

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-app.listen(3000, console.log("¡Servidor encendido!"))
-app.get("/home", (req, res) => {
- res.send("Hello World Express Js")
-})
